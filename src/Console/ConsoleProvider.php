5343aab1e48265463f21ef9b810f695c60918c67
taoran <taoran@han-zi.cn>
2020-02-21 12:02:11 +0800

    provider

--- src/Console/ConsoleProvider.php
+++ src/Console/ConsoleProvider.php
@@ -1,1 +1,36 @@
 <?php
+namespace Taoran\Laravel\Console;
+
+use Illuminate\Support\ServiceProvider;
+
+class ConsoleProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerConsoleCommand();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    public function registerConsoleCommand()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                \Taoran\Laravel\Console\CreateLogicCommand::class
+            ]);
+        }
+    }
+}
