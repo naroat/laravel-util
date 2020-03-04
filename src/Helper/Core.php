25fefe53a23bec9e9fa7dc88588469f1be909f25
taoran <taoran@han-zi.cn>
2020-03-04 18:29:01 +0800

    1

--- src/Helper/Core.php
+++ src/Helper/Core.php
@@ -1,1 +1,14 @@
 <?php
+if (!function_exists('response_json')) {
+    function response_json($data = array(), $list = array(), $code = 200)
+    {
+        $data = [
+            'status' => true,
+            'error_msg' => 'ok',
+            'error_code' => '',
+            'data' => empty($data) ? null : $data,
+            'list' => $list,
+        ];
+        return response()->json($data, $code, $list, JSON_UNESCAPED_UNICODE);
+    }
+}
