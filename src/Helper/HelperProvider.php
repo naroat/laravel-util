e64b770b9ea50da9105ee870d55df118c568ded2
taoran <taoran@han-zi.cn>
2020-03-04 18:53:12 +0800

    1

--- src/Helper/HelperProvider.php
+++ src/Helper/HelperProvider.php
@@ -22,8 +22,8 @@ class HelperProvider extends ServiceProvider
      */
     public function register()
     {
-        var_dump(123);
         require_once __DIR__ . 'Core.php';
+        require_once __DIR__ . 'String.php';
 
     }
 
